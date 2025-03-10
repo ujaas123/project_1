02b4fa32ceeb981c5cfe30cfec79d1dc6ab9af85
Ujaas Sharma <112536147+ujaas123@users.noreply.github.com>
2025-03-10 12:03:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -49,7 +49,7 @@ def submit():
         server.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
         server.sendmail(EMAIL_ADDRESS, EMAIL_ADDRESS, msg.as_string())
 
-    return 'Form submitted successfully!'
+    return 'SOMETHING WENT WRONG!'
 
 if __name__ == '__main__':
     app.run(debug=True)
